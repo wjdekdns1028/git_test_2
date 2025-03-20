439c0d77455c083d7d5e926c0b2302d5aaf64e05
Jung, Daun <wjdekdns0218@gmail.com>
2025-03-20 12:24:35 +0900

    Main.java 수정정

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Hello h = new Hello();
-        h.say();
+        Hello hello = new Hello();
+        hello.say();
     }
 }
